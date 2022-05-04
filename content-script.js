933f4b52cb185c16b4dba7b69e495732b4a76b71
vasker <yarinvasket@gmail.com>
2022-05-04 16:16:52 +0300

    Replace function

---
+++ content-script.js
@@ -0,0 +1,10 @@
+// The function to be replaced
+const func12285 = self.webpackChunkwhatsappweb_client[8][1][12285];
+
+const searchValue = "c=a(8139),",
+replaceValue = "c=a(8139);c.sendReactionsEnabled=function(){return true;};var ";
+
+// The new injected function
+const injectedFuncString = func12285.toString().replace(searchValue, replaceValue);
+// Using eval here is safe, because we are accessing a local variable
+const injectedFunc = eval(injectedFuncString);
