8a7066b9c6cf7e07e379b35ed63eef22462404c7
vasker <yarinvasket@gmail.com>
2022-05-06 12:08:09 +0300

    it works, but in a very weird way...

--- content-script.js
+++ content-script.js
@@ -1,7 +1,20 @@
-let s = document.createElement('script');
-s.src = chrome.runtime.getURL('inject.js');
-s.onload = function() {
-    this.remove();
-};
+function loadScript(url, callback) {
+    let s = document.createElement('script');
+    s.src = url;
+    s.type = "text/javascript";
+    
+    s.onload = function() {
+        this.remove();
+        callback(s);
+    };
 
-(document.head || document.documentElement).appendChild(s);
+    (document.head || document.documentElement).appendChild(s);
+}
+
+// call the function...
+loadScript(chrome.runtime.getURL('bootstrap_main.c195b67ded28e8965831.js'), function(script) {
+    /*const searchValue = "c=a(8139),";
+    const replaceValue = "c=a(8139);c.sendReactionsEnabled=function(){return true;};var "
+
+    script.innerHTML = script.innerHTML.replace(searchValue, replaceValue);*/
+});
